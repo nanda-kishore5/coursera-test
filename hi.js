262c3c81fd98418679f35955e10d564b9d257e31
nanda-kishore5 <51995588+nanda-kishore5@users.noreply.github.com>
2020-06-12 12:23:30 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,15 @@
+(function () {
+
+var names = ["Yamini", "Joker",  "Jyothi", "Nanda", "Dpp", "Kll"];
+for(var i=0;i<names.length;i++){
+	var firstLetter = names[i].charAt(0).toLowerCase();
+
+
+if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
